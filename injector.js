fe043a6667cc0ba41d767fcdf74391455316c4f7
Matthew Bryant (mandatory) <matt@refinery.io>
2021-01-10 17:26:47 -0800

    Finished up some things and added ability to enumerate extensions and their permissions

--- injector.js
+++ injector.js
@@ -3,6 +3,19 @@ const { Command } = require('commander');
 const util = require('util');
 const fs = require('fs');
 
+const EXTENSION_ENUMERATION_SCRIPT = `
+function get_chrome_extensions() {
+	return new Promise(function(resolve, reject) {
+		chrome.management.getAll((extensions) => {
+			resolve(extensions);
+		});
+	});
+}
+(async () => {
+	const unique_name_extension_data = await get_chrome_extensions();
+	return JSON.stringify(unique_name_extension_data);
+})();`;
+
 const wait = ms => new Promise(res => setTimeout(res, ms));
 
 const program_banner = `
@@ -27,11 +40,76 @@ const program_banner = `
 	.option('-p, --port <number>', 'Port which remote debugging is hosted on (default 9222).', 9222)
 	.option('-m, --method <value>', 'The method of injection. Works either by evaluating in an existing page ("existing") or by creating a new page in the background for the origin ("create") if one does not exist. Default is "existing" which will fail if no pages exist with the origin specified. Note that this will *not* affect the original script as the injected script is run in an isolated world.', 'existing')
 	.option('-c, --cleanup', 'Only available if using the "create" method. Closes out the created page after injecting the script.', false)
-	.requiredOption('-s, --script <value>', 'Either a path to a JavaScript file or inline JavaScript to execute in the specified origin.')
-	.requiredOption('-o, --origin <value>', 'The origin to inject the JavaScript into, such as https://example.com or chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm')
-	.option('-q, --quiet', 'Don\'t print any output during execution.', false)
+	.option('-s, --script <value>', 'Either a path to a JavaScript file or inline JavaScript to execute in the specified origin.')
+	.option('-o, --origin <value>', 'The origin to inject the JavaScript into, such as https://example.com or chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm')
+	.option('-l, --list', 'List currently installed extensions and their permissions.', false)
 
 	program.parse(process.argv);
+
+	// Host settings for connecting to Chrome/Chromium
+	const host_settings = {
+		host: program.host,
+		port: program.port
+	};
+
+	const cdp_client = await CDP({
+		host: host_settings.host,
+		port: host_settings.port
+	});
+
+	if(!(program.script && program.origin) && !program.list) {
+		console.error(`Error, both --script and --origin are required options unless --list is specified.`);
+		process.exit();
+	}
+
+	// List extensions and permissions
+	if(program.list) {
+		const target_origin = `chrome://extensions`;
+		const target_origin_metadata = await create_new_target_with_origin(
+			cdp_client,
+			host_settings,
+			target_origin
+		);
+
+		const target_id = target_origin_metadata.targetId;
+
+		const extension_enumeration_result = await inject_script_into_target(
+			target_id,
+			host_settings,
+			EXTENSION_ENUMERATION_SCRIPT,
+			false
+		);
+
+		const extensions_array = JSON.parse(extension_enumeration_result.value);
+
+		await close_new_window(
+			target_id,
+			host_settings
+		);
+
+		const formatted_extensions = extensions_array.map(extension_data => {
+			const combined_permissions = extension_data.hostPermissions.concat(
+				extension_data.permissions
+			);
+
+			console.log(`ID: ${extension_data.id}`);
+			console.log(`Name: ${extension_data.name}`);
+			console.log(`Description: ${extension_data.description}`);
+			console.log(`Enabled: ${extension_data.enabled}`);
+			console.log(`Permissions:`);
+
+			if(combined_permissions.length > 0) {
+				combined_permissions.map(permission => {
+					console.log(`* ${permission}`);
+				});
+			} else {
+				console.log(`<No permissions declared>`);
+			}
+			console.log(`\n--- \n`);
+		});
+
+		process.exit();
+	}
 
 	// Don't allow specifying "cleanup" and method "existing"
 	if(program.cleanup && program.method === 'existing') {
@@ -49,11 +127,6 @@ const program_banner = `
 	// Resolve the specified script
 	const script_to_inject = await resolve_script(program.script);
 
-	// Only print the banner if the user didn't specify quiet mode.
-	if(!program.quiet) {
-		console.log(program_banner);
-	}
-
 	const formatted_target_origin = (/^[a-p]{32}$/g.test(program.origin) ? `chrome-extension://${program.origin}` : program.origin);
 	const target_origin = is_valid_origin(formatted_target_origin);
 
@@ -62,17 +135,6 @@ const program_banner = `
 		console.error(`Please specify a valid origin, e.g: https://example.com, chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm`);
 		process.exit(-1);
 	}
-
-	// Host settings for connecting to Chrome/Chromium
-	const host_settings = {
-		host: program.host,
-		port: program.port
-	};
-
-	const cdp_client = await CDP({
-		host: host_settings.host,
-		port: host_settings.port
-	});
 
 	let target_origin_metadata = await get_target_with_matching_origin(
 		cdp_client,
@@ -97,17 +159,13 @@ const program_banner = `
 		is_created_window = true;
 	}
 
-	console.log(`Target we're going after: `);
-	console.log(target_origin_metadata);
-
 	const target_id = target_origin_metadata.targetId;
-
-	console.log(`Target ID is ${target_id}`);
 
 	const injection_result = await inject_script_into_target(
 		target_id,
 		host_settings,
-		script_to_inject
+		script_to_inject,
+		false
 	);
 
 	console.log(`*** Result of your injected script ***`);
@@ -119,19 +177,23 @@ const program_banner = `
 	// asked for us to close it out after the script
 	// has finished running, we close the window.
 	if(is_created_window && program.cleanup) {
-		const new_window_client = await CDP({
-			target: target_id,
-			host: host_settings.host,
-			port: host_settings.port
-		});
-
-		const {Page} = new_window_client;
-
-		const close_result = await Page.close();
+		await close_new_window(target_id, host_settings);
 	}
 
 	process.exit(0);
 })();
+
+async function close_new_window(target_id, host_settings) {
+	const new_window_client = await CDP({
+		target: target_id,
+		host: host_settings.host,
+		port: host_settings.port
+	});
+
+	const {Page} = new_window_client;
+
+	const close_result = await Page.close();
+}
 
 async function create_new_target_with_origin(cdp_client, host_settings, input_origin) {
 	const { Target } = cdp_client;
@@ -162,7 +224,18 @@ async function create_new_target_with_origin(cdp_client, host_settings, input_or
 		port: host_settings.port
 	});
 
-	const {Browser, Page, Fetch} = new_window_client;
+	const {Page, Fetch} = new_window_client;
+
+	// Don't do the interception routine if it's not a web origin
+	if(!(input_origin.startsWith('http://') || input_origin.startsWith('https://'))) {
+		// Navigate to the origin the user specified...
+		const page_navigate_promise = Page.navigate({
+			url: input_origin
+		});
+		await hide_window(new_window_client);
+
+		return new_target_metadata;
+	}
 
 	request_paused_event_promise = Fetch.requestPaused();
 
@@ -203,6 +276,11 @@ async function create_new_target_with_origin(cdp_client, host_settings, input_or
 	// No need for any further interception
 	await Fetch.disable();
 
+	return new_target_metadata;
+}
+
+async function hide_window(new_window_client) {
+	const {Browser} = new_window_client;
 	const new_window_metadata = await Browser.getWindowForTarget();
 	const new_window_id = new_window_metadata.windowId;
 
@@ -224,8 +302,6 @@ async function create_new_target_with_origin(cdp_client, host_settings, input_or
 			windowState: 'minimized',
 		}
 	});
-
-	return new_target_metadata;
 }
 
 async function get_target_with_matching_origin(cdp_client, target_origin) {
@@ -257,8 +333,6 @@ function is_valid_origin(input_origin) {
 	} catch (e) {
 		return false;
 	}
-
-	console.log(url_object);
 
 	let has_valid_protocol_handler = false;
 
@@ -307,7 +381,7 @@ async function attempt_file_script_resolve(filename_or_script) {
 	});
 }
 
-async function inject_script_into_target(target_id, host_settings, script_to_inject) {
+async function inject_script_into_target(target_id, host_settings, script_to_inject, isolation_enabled) {
 	const extension_target_client = await CDP({
 		target: target_id,
 		host: host_settings.host,
@@ -316,25 +390,23 @@ async function inject_script_into_target(target_id, host_settings, script_to_inj
 
 	const {Target, Page, Runtime} = extension_target_client;
 
-	const frame_tree = await Page.getFrameTree({});
-
-	console.log(`Page tree:`);
-	console.log(frame_tree);
-
-	const isolated_world_data = await Page.createIsolatedWorld({
-		frameId: frame_tree.frameTree.frame.id,
-	});
-
-	console.log(`Isolated world: `);
-	console.log(isolated_world_data);
-
-	const execution_context_id = isolated_world_data.executionContextId;
-
-	const evaluate_results = await Runtime.evaluate({
-		contextId: execution_context_id,
+	let runtime_params = {
 		expression: script_to_inject,
 		awaitPromise: true,
-	});
+	}
+
+	if(isolation_enabled) {
+		const frame_tree = await Page.getFrameTree({});
+
+		const isolated_world_data = await Page.createIsolatedWorld({
+			frameId: frame_tree.frameTree.frame.id,
+		});
+
+		const execution_context_id = isolated_world_data.executionContextId;
+		runtime_params['contextId'] = execution_context_id;
+	}
+
+	const evaluate_results = await Runtime.evaluate(runtime_params);
 
 	return evaluate_results.result;
 }
